6c10db2ad523436cb573594f6a2f5b7cc488fa3c
Brian-Agwenge1997 <otienohagwenge@gmail.com>
2024-02-09 12:52:53 +0300

    change width

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ const headerImage = document.getElementById("header-image")
 
 let courseTitle = "Forex Factory"
 
-let newImage = `<img id="header-image" src="https://play-lh.googleusercontent.com/aw-F9mcy_TywRskVTahXgMr75mlj9sgiV5SnNuZgEy9Yc8LQ65AylNbM64BqQrnCxKnL=w240-h480-rw" width="100%">`
+let newImage = `<img
+ id="header-image" 
+ src="https://play-lh.googleusercontent.com/aw-F9mcy_TywRskVTahXgMr75mlj9sgiV5SnNuZgEy9Yc8LQ65AylNbM64BqQrnCxKnL=w240-h480-rw"
+  width="10%">`
 
 
-function changeHeroSection(){
-    headerTitle.innerText =  courseTitle
-     headerImage.innerHTML =newImage
-    
+function changeHeroSection() {
+    headerTitle.innerText = courseTitle
+    headerImage.innerHTML = newImage
+
 }
